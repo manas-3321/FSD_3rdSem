f6f4858be5f76134a5f568c927c21e0b0361a7f8
Manas Kumar <manas.mr.26@gmail.com>
2026-09-22 10:00:34 +0530

    lab file

--- exp2.js
+++ exp2.js
@@ -1,18 +1,24 @@
-// const fs = require('fs');
-
-// //update (append)
-// fs.appendFile('example.txt', '\nThis line was added.', (err)=> {
-//     if(err) throw err;
-//     console.log('File updated (appended)!');
-
-// });
-
-
 const fs = require('fs');
-//delete
-fs.unlink('example.txt', (err) => {
-    if(err) throw err;
-    console.log('file deleted!');
-}
-);
 
+fs.writeFile("example.txt", "Hello World!", (err) => {
+    if (err) throw err;
+
+    console.log("File created!");
+
+    fs.readFile("example.txt", "utf8", (err, data) => {
+        if (err) throw err;
+
+        console.log("File content:", data);
+
+        fs.writeFile("example.txt", "This is the updated content", (err) => {
+            if (err) throw err;
+
+            console.log("File overwritten(updated)!");
+
+            fs.unlink('example.txt' , (err) => {
+                if(err) throw err;
+                console.log('File deleted!');
+            });
+        });
+    });
+});
